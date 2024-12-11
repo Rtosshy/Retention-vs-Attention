3e0eb4beafdbacadc008eb640e2667ee2871cc28
Rtosshy <tosshy820gmail.com>
2024-12-11 18:45:23 +0900

    completion of the train template

--- train.py
+++ train.py
@@ -1,36 +1,52 @@
 import torch
-from torch.utils.data import DataLoader
+from torch.optim import AdamW
 from datasets import load_dataset
-from transformers import BertTokenizer, DataCollatorWithPadding, AdamW
-from test_transformer_mlm import TransformerMLM
+from preprocess import exec_preprocess
+from transformer_mlm import TransformerMLM
 
-raw_datasets = load_dataset("bookcorpus", split="train[:10%]")
+raw_datasets = load_dataset("bookcorpus", split="train[:1%]") # {text}
 checkpoint = "bert-base-uncased"
-tokenizer = BertTokenizer.from_pretrained(checkpoint)
+raw_datasets = raw_datasets.filter(lambda x: len(x["text"]) < 16)
 
-def tokenize_function(example):
-    return tokenizer(example["text"], truncation=True)
+tokenizer, dataloaders = exec_preprocess(raw_datasets, checkpoint)
 
-tokenized_datasets = raw_datasets.map(tokenize_function, batched=True)
-data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
-
-train_testvalid = tokenized_datasets.train_test_split(test_size=0.2)
-valid_test = train_testvalid['test'].train_test_split(test_size=0.5)
-
-train_dataset = train_testvalid['train'] # {text, inpt_ids, token_type_ids, attention_mask}
-valid_dataset = valid_test['train']
-test_dataset = valid_test['test']
-
-train_dataloader = DataLoader(train_dataset, shuffle=True, batch_size=8, collate_fn=data_collator)
-valid_dataloader = DataLoader(valid_dataset, batch_size=8, collate_fn=data_collator)
-test_dataloader = DataLoader(test_dataset, batch_size=8, collate_fn=data_collator)
+train_dataloader, valid_dataloader, test_dataloader = dataloaders
 
 vocab_size = tokenizer.vocab_size
 d_model = 512
 n_heads = 8
 n_layers = 6
-max_seq_len = tokenizer.model_max_length
+max_seq_len = 16 # tokenizer.model_max_length
 batch_size = 8
 
+model = TransformerMLM(vocab_size=vocab_size, d_model=d_model, n_heads=n_heads, n_layers=n_layers, max_seq_len=max_seq_len)
 
-model = TransformerMLM()
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+optimizer = AdamW(model.parameters(), lr=5e-5)
+criterion = torch.nn.CrossEntropyLoss()
+
+def train(epoch, model, optimizer, train_dataloader, valid_dataloader):
+    model.train()
+    for i in range(epoch):
+        print(f"Epoch {i}")
+        j = 0
+        for batch in train_dataloader:
+            print(f"Batch {j}")
+            input_ids = batch['input_ids'].to(device)
+            attention_mask = batch['attention_mask'].to(device)
+            labels = batch['labels'].to(device)
+            outputs = model(input_ids, attention_mask)
+
+            loss = criterion(outputs.view(-1, vocab_size), labels.view(-1))
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+            print(f"Epoch {i} Batch {j} Loss: {loss.item()}")
+            j += 1
+
+        print(f"Epoch {i} Loss: {loss.item()}")
+    model.eval()
+
+
+train(1, model, optimizer, train_dataloader, valid_dataloader)
